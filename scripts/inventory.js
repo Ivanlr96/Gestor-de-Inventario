b630bc70274f2c0af33b31b9fd4a43e0aff841e7
unknown <ivanlorenzoruiz@alumnos.tuniversformacion.es>
2023-11-08 00:11:22 +0100

    Creado el archivo inventory, que contiene el array del inventario existente

---
+++ scripts/inventory.js
@@ -0,0 +1,10 @@
+// Array del inventario al que se van añadiendo los nuevos productos
+export const inventory = [
+
+    { id: 1, nombre: "Teclado 1", categoria: "Periféricos", cantidad: 2, precio: 80 },
+    { id: 2, nombre: "Teclado 2", categoria: "Periféricos", cantidad: 2, precio: 70 },
+    { id: 3, nombre: "Intel I5 12400F", categoria: "Procesadores", cantidad: 2, precio: 250 },
+    { id: 4, nombre: "Amd Ryzen 5", categoria: "Procesadores", cantidad: 2, precio: 300 },
+    { id: 5, nombre: "Nvidia rtx 4060", categoria: "Tarjetas gráficas", cantidad: 2, precio: 350 },
+    { id: 6, nombre: "Radeon rx 7700", categoria: "Tarjetas gráficas", cantidad: 2, precio: 500 },
+];
